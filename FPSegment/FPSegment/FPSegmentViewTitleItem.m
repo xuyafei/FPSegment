92842a3f43bdcdb81414822a152244f96c3dd23a
xuyafei <xuyafei0208@126.com>
2017-06-10 22:55:28 +0800

    构建工程

--- FPSegment/FPSegment/FPSegmentViewTitleItem.m
+++ FPSegment/FPSegment/FPSegmentViewTitleItem.m
@@ -7,8 +7,93 @@
 //
 
 #import "FPSegmentViewTitleItem.h"
+#import "UIView+Sizes.h"
+#import "FPSegmentDefines.h"
+
+#define _defaultFont [UIFont systemFontOfSize:15]
+#define _defaultTitleColor_Normal     [UIColor blackColor]
+#define _defaultTitleColor_Highlight  [UIColor redColor]
+
+#define _MinWidth  32
+#define _MaxWidth  FPMainScreenWidth / 2
+
+@interface FPSegmentViewTitleItem() {
+    BOOL _hasNormalColor;
+    BOOL _hasHighlightColor;
+    BOOL _touchedFlag;
+    id   _target;
+    SEL  _action;
+}
+
+@property(nonatomic, strong) UILabel *titleLabel;
+
+@end
 
 @implementation FPSegmentViewTitleItem
 
+- (instancetype)initWithFrame:(CGRect)frame title:(NSString *)title {
+    self = [super initWithFrame:frame];
+    
+    if(self) {
+        self.space = 8;
+        self.title = title;
+    }
+    
+    return self;
+}
 
+- (void)layoutSubviews {
+    CGFloat buttonWidth = [self calcuWidth];
+    self.frame = CGRectMake(self.x, self.y, buttonWidth + self.space, self.height);
+    self.titleLabel.frame = CGRectMake(self.space / 2, 0, buttonWidth, self.height);
+}
+
+- (CGFloat)calcuWidth {
+    if(self.title == nil) {
+        return _MinWidth;
+    }
+    
+    UIFont *font = self.font == nil ? _defaultFont : self.font;
+    CGRect frame = [self.title boundingRectWithSize:CGSizeMake(_MaxWidth, self.height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil];
+    CGFloat width = frame.size.width;
+    
+    return width > _MinWidth ? width : _MinWidth;
+}
+
++ (CGFloat)calcuWidth:(NSString *)title {
+    FPSegmentViewTitleItem *item = [[FPSegmentViewTitleItem alloc] initWithFrame:CGRectZero title:title];
+    return [item calcuWidth] + item.space;
+}
+
+- (void)setNormalColor:(UIColor *)normalColor {
+    _normalColor = normalColor;
+    _hasNormalColor = YES;
+    [self setHighlight:_highlight];
+}
+
+- (void)setHighlightColor:(UIColor *)highlightColor {
+    _highlightColor = highlightColor;
+    _hasHighlightColor = YES;
+    [self setHighlight:_highlight];
+}
+
+- (void)setTitle:(NSString *)title {
+    _title = title;
+    self.titleLabel.text = title;
+    [self setNeedsLayout];
+}
+
+- (void)setFont:(UIFont *)font {
+    self.titleLabel.font = font;
+}
+
+- (void)setSpace:(CGFloat)space {
+    _space = space;
+    [self setNeedsLayout];
+}
+
+- (void)setHighlight:(BOOL)highlight {
+    _highlight = highlight;
+    self.titleLabel.textColor = highlight == YES ? _hasHighlightColor == YES ? self.highlightColor : _defaultTitleColor_Highlight : _hasNormalColor == YES ? self.normalColor : _defaultTitleColor_Normal;
+}
 @end
